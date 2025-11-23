7f52c433044475c483a11fa324b55a77603bbbf8
Skjalg G. Eriksen <skjalg.eriksen@gmail.com>
2025-11-24 00:11:52 +0100

    implement enable/disable for new dot manager

---
+++ dots.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from re import Pattern
+from sys import argv
+from pathlib import Path
+from typing import List, Tuple, Iterator
+from os import walk
+from re import compile as re_compile
+
+# dots.py
+# dots -> tui with interactive checklist, [q] to quit, [x] to execute, [enter] to toggle config
+# dots --list -> lists active configs
+# dots --enable <name>
+# dots --disable <name>
+
+HOME = Path.home()
+DOTFILES = Path(__file__).resolve().parent
+IGNORE = DOTFILES / ".dotsignore"
+CONFIG_DIR = DOTFILES / ".config"
+
+
+def load_ignore() -> List[Pattern]:
+    patterns: List[Pattern] = []
+    if IGNORE.exists():
+        for line in IGNORE.read_text().splitlines():
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            patterns.append(re_compile(line))
+
+    return patterns
+
+
+def is_enabled(source_root: Path) -> bool:
+    target = HOME / source_root.relative_to(DOTFILES)
+    return target.is_symlink() and target.resolve() == source_root.resolve()
+
+
+def itr_dotfiles() -> Iterator[Tuple[bool, Path]]:
+    """
+    Discover and return all available configuration modules.
+
+    Returns
+    -------
+    List[(bool, Path)]
+        A list of tuples with enabled/disabled and relative
+        paths inside the .dotfiles directory that represent
+        individual configuration modules.
+
+    Notes
+    -----
+    - `.config/<module>/` directories are treated as folder-level modules and
+      will be symlinked as whole directories.
+    - All other files in modules are treated as file-level links.
+    - Any paths listed in `.ignore` should be excluded from the result.
+    """
+    ignore_patterns = load_ignore()
+
+    def is_ignored(path: str):
+        return any(regex.search(path) for regex in ignore_patterns)
+
+    for root, _, files in walk(DOTFILES, topdown=True):
+        if is_ignored(root):
+            continue
+        root_path = Path(root)
+
+        # handle .config modules as dir symlinks
+        if root_path.parent == CONFIG_DIR:
+            yield (is_enabled(root_path), root_path)
+
+        # otherwise skip .config
+        if root_path.is_relative_to(CONFIG_DIR):
+            continue
+
+        # handle normal files
+        for f in files:
+            if is_ignored(f):
+                continue
+
+            file_path = root / Path(f)
+            yield (is_enabled(file_path), file_path)
+
+
+def disable(relative_path: str):
+    """
+    Disable (uninstall) a configuration module by removing its symlinks.
+
+    Parameters
+    ----------
+    relative_path : str
+        The module's path relative to the .dotfiles directory.
+
+    Behavior
+    --------
+    - Removes symlinks previously created for this module under $HOME.
+    - Restores any corresponding `{path}.bak` files if they exist.
+    - Cleans up empty directories left after removing symlinks.
+    """
+    source_root = DOTFILES / Path(relative_path)
+    assert source_root.exists(), "config not found"
+
+    target = HOME / source_root.relative_to(DOTFILES)
+    if not is_enabled(source_root):
+        print("config not enabled")
+        return
+    assert target.is_symlink, "an enabled config must be a symlink"
+
+    target.unlink()
+    target_bak = target.with_suffix(".bak")
+
+    if target_bak.exists():
+        target_bak.rename(target)
+
+
+def enable(relative_path: str):
+    """
+    Activate (install) a configuration module by creating symlinks in $HOME.
+
+    Parameters
+    ----------
+    relative_path : str
+        The module's path relative to the .dotfiles directory.
+
+    Behavior
+    --------
+    - If a target file already exists in $HOME, it is renamed to `{path}.bak`.
+    - A symlink is created from the dotfiles module to the corresponding
+      location in $HOME.
+    - Directory-level linking is performed only for `.config/<module>/`,
+      all other files are linked individually.
+    """
+    source_root = DOTFILES / Path(relative_path)
+    assert source_root.exists(), "config not found"
+
+    target = HOME / source_root.relative_to(DOTFILES)
+    if is_enabled(source_root):
+        print(f"config {relative_path} is already enabled")
+        return
+
+    if target.exists():
+        target.rename(f"{target}.bak")
+
+    target.symlink_to(source_root)
+
+
+def main():
+    if len(argv) == 1:
+        print("launch tui")
+        return
+
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--list", action="store_true", help="Lists configs and their status"
+    )
+    parser.add_argument(
+        "--enable", metavar="NAME", help="Enable (creates symlink in the system)"
+    )
+    parser.add_argument(
+        "--disable", metavar="NAME", help="Disable (removes symlink in the system)"
+    )
+
+    args = parser.parse_args()
+
+    if args.list:
+        for enabled, path in itr_dotfiles():
+            status = " [ENABLED] " if enabled else "[DISABLED] "
+            print(f"{status}{path.relative_to(DOTFILES)}")
+        return
+
+    if args.enable:
+        enable(args.enable)
+        return
+
+    if args.disable:
+        disable(args.disable)
+        return
+
+
+if __name__ == "__main__":
+    main()
