00cf8cdf64278f21be596808035a3902b153eba6
Skjalg G. Eriksen <skjalg.eriksen@gmail.com>
2025-12-07 00:05:10 +0100

    wip emacs transparency in the terminal

--- .config/doom/lisp/colors.el
+++ .config/doom/lisp/colors.el
@@ -8,15 +8,17 @@
 ;; (setq doom-theme 'doom-ayu-mirage)
 ;; (setq doom-theme 'doom-spacegrey)
 
+;; (unless (display-graphic-p)
+;;   (set-face-attribute 'default nil :background "unspecified-bg"))
 
-;; (add-hook 'after-make-frame-functions
-;;           (lambda (frame)
-;;             (when (not (display-graphic-p frame))
-;;               (with-selected-frame frame
-;;                 (set-face-attribute 'default nil :background "unspecified-bg")))))
+;; (add-hook 'after-init-hook
+;;           (lambda ()
+;;             (unless (display-graphic-p)
+;;               (set-face-attribute 'default nil :background "unspecified-bg"))))
 
+;; (set-face-background 'default "undefined")
+;; (set-face-attribute 'default nil :background "unspecified-bg")
 
-(add-hook 'after-make-frame-functions 'on-frame-open)
 
 (set-frame-parameter nil 'alpha-background 80)
 (add-to-list 'default-frame-alist '(alpha-background . 80))
