c59cbb28ed12f58b1dee6a1822b9a7673312d32d
Skjalg Gustav Eriksen <skjalg.eriksen@bouvet.no>
2026-04-01 19:12:40 +0200

    feat: add treemacs to emacs config

--- .emacs.d/lisp/tools.el
+++ .emacs.d/lisp/tools.el
@@ -1,6 +1,16 @@
 (use-package magit
   :bind (("C-x g" . magit-status)))
 
+(use-package treemacs
+  :bind (("C-x t t" . treemacs)
+         ("C-x t 1" . treemacs-select-window)))
+
+(use-package treemacs-evil
+  :after (treemacs evil))
+
+(use-package treemacs-magit
+  :after (treemacs magit))
+
 (use-package which-key
   :init (which-key-mode)
   :config (setq which-key-idle-delay 0.3))
