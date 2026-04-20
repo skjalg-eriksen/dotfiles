4018d5982bd2781034d4abce544af93cdf9e98dc
Skjalg Gustav Eriksen <skjalg.eriksen@gmail.com>
2026-04-21 00:57:19 +0200

    feat: undo + gcc commentary mode

--- .emacs.d/lisp/modal-editing.el
+++ .emacs.d/lisp/modal-editing.el
@@ -1,7 +1,8 @@
 (use-package evil
   :init
   (setq evil-want-C-u-scroll t
-        evil-want-keybinding nil)
+        evil-want-keybinding nil
+	evil-undo-system 'undo-redo)
   :config
   (evil-mode 1))
 
@@ -9,5 +10,33 @@
   :after evil
   :config
   (evil-collection-init))
+
+(use-package evil-commentary
+  :after evil
+  :hook (prog-mode . evil-commentary-mode)  :config
+  (evil-commentary-mode))
+
+(use-package evil-mc
+  :after evil
+  :config
+  (global-evil-mc-mode 1))
+
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "U") 'evil-redo))
+
+;; multi cursor setup
+(with-eval-after-load 'evil-mc
+  ;; like C-> (next match)
+  (define-key evil-normal-state-map (kbd "C->") 'evil-mc-make-and-goto-next-match)
+  
+  ;; like C-< (prev match)
+  (define-key evil-normal-state-map (kbd "C-<") 'evil-mc-make-and-goto-prev-match)
+
+  ;; like mark all
+  (define-key evil-normal-state-map (kbd "C-c C-<") 'evil-mc-make-all-cursors)
+
+  ;; edit lines (closest equivalent)
+  (define-key evil-visual-state-map (kbd "C-S-c C-S-c") 'evil-mc-make-cursor-in-visual-selection-beg)
+)
 
 (provide 'modal-editing)
