6ea43522e77b4d3c7dc88a38fb93a69deab1a304
Skjalg Gustav Eriksen <skjalg.eriksen@gmail.com>
2026-04-21 00:45:19 +0200

    feat: remove multiple-cursors from editing.el

--- .emacs.d/lisp/editing.el
+++ .emacs.d/lisp/editing.el
@@ -5,10 +5,4 @@
 (use-package expand-region
   :bind (("C-=" . er/expand-region)))
 
-(use-package multiple-cursors
-  :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C->"         . mc/mark-next-like-this)
-         ("C-<"         . mc/mark-previous-like-this)
-         ("C-c C-<"     . mc/mark-all-like-this)))
-
 (provide 'editing)
