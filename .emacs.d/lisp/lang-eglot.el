90f3ebbfff1183890d2900bbad04bfa72479c17c
Skjalg Gustav Eriksen <skjalg.eriksen@gmail.com>
2026-04-21 00:41:21 +0200

    feat: add haskell eglot mode

--- .emacs.d/lisp/lang-eglot.el
+++ .emacs.d/lisp/lang-eglot.el
@@ -6,13 +6,15 @@
          (c-mode      . eglot-ensure)
          (c++-mode    . eglot-ensure)
          (asm-mode    . eglot-ensure)
-         (sh-mode     . eglot-ensure))
+         (sh-mode     . eglot-ensure)
+	 (haskell-mode . eglot-ensure))
   :config
   (setq eglot-autoshutdown t))
 
 (use-package rust-mode)
 (use-package zig-mode)
 (use-package nasm-mode)
+(use-package haskell-mode)
 
 (use-package exec-path-from-shell
   :config
