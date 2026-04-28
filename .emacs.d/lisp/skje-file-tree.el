f921a67ddaa0d5baf63685dfbaf51e787f1b90e8
Skjalg Gustav Eriksen <skjalg.eriksen@gmail.com>
2026-04-29 00:08:41 +0200

    feat: simplfy neotree config

--- .emacs.d/lisp/skje-file-tree.el
+++ .emacs.d/lisp/skje-file-tree.el
@@ -10,35 +10,10 @@
 ;;   :after (treemacs magit))
 
 
-(use-package neotree
-  :config
-  (defun skje-neotree--project-root ()
-    "Return git root for current buffer, or fallback to its directory."
-    (let* ((file (or (buffer-file-name) default-directory))
-           (dir (if (file-directory-p file)
-                    file
-                  (file-name-directory file))))
-      (or (locate-dominating-file dir ".git")
-          dir))))
-
-  (defun skje-neotree-root ()
-    "Toggle NeoTree rooted at current buffer's project root."
-    (interactive)
-    (let ((root (skje-neotree--project-root))
-          (file (buffer-file-name)))
-      (if (neo-global--window-exists-p)
-          (neotree-hide)
-        (progn
-          (neotree-dir root)
-          (when file
-            (neo-open-file file))))))
+(use-package neotree)
 
 (with-eval-after-load 'evil
   (define-key evil-normal-state-map (kbd "SPC e")
-    #'skje-neotree-root))
-
-  
-;; (with-eval-after-load 'evil
-;;   (define-key evil-normal-state-map (kbd "SPC e") #'skje-neotree-root))
+    #'neotree-toggle))
 
 (provide 'skje-file-tree)
