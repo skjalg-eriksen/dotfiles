abd4b4f8a243106ba45b8dfcf4ff18fba231eede
Skjalg Gustav Eriksen <skjalg.eriksen@gmail.com>
2026-05-15 00:11:49 +0200

    feat: nvim language spesific configs

---
+++ .config/nvim/ftplugin/cs.lua
@@ -0,0 +1,7 @@
+-- if true then return end
+
+vim.opt_local.expandtab = true
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.commentstring = '// %s'
